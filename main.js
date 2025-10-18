99c7b964a8b1ac3bfc1a4f9fdd3a3ca41e7fb178
Patrick <pfriis@outlook.com>
2025-10-18 13:53:02 +0200

    L5 Challenge: Add array of sizes and display as list with v-for

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['Small', 'Medium', 'Large', 'X-Large'],
             variants: [
                 { id: 2234, color: 'green'},
                 { id: 2235, color: 'blue'},
