76ce88b5987bcbdc7a2db4ebd171107182b99675
Patrick <pfriis@outlook.com>
2025-10-19 16:00:40 +0200

    L9: Copy data(), methods and computed from main.js to ProductDisplay.js

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -26,5 +26,37 @@ app.component('product.display' {
             <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Add to Cart</button>
           </div>
         </div>
-      </div>`
+      </div>`,
+          data() {
+        return {
+            cart: 0,
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+    }
 })
