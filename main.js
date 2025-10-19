76c647ce968204809c7c1cdbf25d5fb69575e956
Patrick <pfriis@outlook.com>
2025-10-19 15:43:13 +0200

    L8 Challenge: Add onSale boolean and display using computed property when onSale is true

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
+            onSale: true,
             selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
@@ -29,6 +30,9 @@ const app = Vue.createApp({
         },
         inStock() {
             return this.variants[this.selectedVariant].quantity
+        },
+        isOnSale() {
+            return this.brand + ' ' + this.product + ' ' + "is on sale!"
         }
     }
 })
