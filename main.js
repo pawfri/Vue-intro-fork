c9d8a92ce4233e7521b6146d3264e1874b44e6ab
Patrick <pfriis@outlook.com>
2025-10-25 23:45:11 +0200

    L10 Challenge: Add button to ProductDisplay, which removes product from cart

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ const app = Vue.createApp({
     methods: {
         updateCart(id) {
             this.cart.push(id)
-        }
+        },
+        //Removes the specific id, no matter where it is in the array (safer)
+        removeFromCart(id) {
+            const index = this.cart.indexOf(id)
+                if (index > -1) {
+                this.cart.splice(index, 1)          
+            }
+        },
+        //Alternative remove method: Always remove from the back of the array (simpler but unsafe)
+        // removeFromCart(id) {
+        //     this.cart.pop(id)         
+        // }
     }
 })
