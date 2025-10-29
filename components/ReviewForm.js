b6b1a7b03b05598dc5afe71a20fe682ed53c0eca
Patrick <pfriis@outlook.com>
2025-10-29 09:07:30 +0100

    L11: Add listener @submit with prevent modifier and implement onSubmit method. Import to index.html

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,7 +1,7 @@
 app.component('review-form', {
 template:
   /*html*/
-  `<form class="review-form">
+  `<form class="review-form" @submit.prevent="onSubmit">
     <h3>Leave a review</h3>
     <label for="name">Name:</label>
     <input id="name" v-model="name">
@@ -26,6 +26,20 @@ template:
       name: '',
       review: '',
       rating: null
+    }
+  },
+  methods: {
+    onSubmit() {
+        let productReview = {
+          name: this.name, 
+          review: this.review, 
+          rating: this.rating
+        }
+        this.$emit('review-submitted', productReview)
+
+        this.name = ''
+        this.review = ''
+        this.rating = null
     }
   }   
 })
